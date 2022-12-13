023fb91cb1ddaa3762ecddc670c0a451fe2ce6bd
ojogal <ojog.al@gmail.com>
2022-12-13 14:58:41 +0200

    Associates products and orders with a placements model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,8 @@
 class Order < ApplicationRecord
   validates :total, numericality: { greater_than_or_equal_to: 0 }
   validates :total, presence: true
-  
+
+  has_many :placements, dependent: :destroy
+  has_many :products, through: :placements
   belongs_to :user
 end
